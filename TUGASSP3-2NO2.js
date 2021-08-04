83c82333f4b94f112ba6ca53a1103b45cd64d770
muklisxtc24 <87222847+muklisxtc24@users.noreply.github.com>
2021-08-04 21:25:09 +0700

    Add files via upload

---
+++ TUGASSP3-2NO2.js
@@ -0,0 +1,13 @@
+const goNumber = () => {
+        goNumber2()
+    }
+    
+    const goNumber2 =() => {
+        let kodeUnix = [23, 89,67,29,192,6,2,129,21,872,891,901,70,61,78,62,32,90,90]
+         console.log(kodeUnix.filter(a => a >= 60));
+         
+    
+        }
+       goNumber(goNumber2)
+
+
